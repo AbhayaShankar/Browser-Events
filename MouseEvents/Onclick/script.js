c84c99b9ebcfc81f431639008750a38604fa0732
Abhaya <shankarabhaya9@gmail.com>
2022-11-11 14:03:52 +0530

    accessing innerHTML on onclick

--- MouseEvents/Onclick/script.js
+++ MouseEvents/Onclick/script.js
@@ -5,6 +5,7 @@ function countRabbits() {
 }
 
 let count = 0;
+//incrementing the number of rabbit counts when onCLick event is called .
 elem.onclick = function countRabbits2() {
   count++;
   console.log("Current Count of Rabbits :" + count);
