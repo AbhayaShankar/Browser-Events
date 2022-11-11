d3227f641dd21873145848138e304ae6dd195ec9
Abhaya <shankarabhaya9@gmail.com>
2022-11-11 22:23:29 +0530

    mouse events

--- MouseEvents/Onclick/script.js
+++ MouseEvents/Onclick/script.js
@@ -10,3 +10,29 @@ elem.onclick = function countRabbits2() {
   count++;
   console.log("Current Count of Rabbits :" + count);
 };
+
+function sayThanks() {
+  console.log("Thanks!");
+}
+
+elem.onclick = sayThanks;
+
+//mouseup and mousedown event
+class Menu {
+  handleEvent(event) {
+    switch (event.type) {
+      case "mousedown":
+        btn.innerHTML = "mouse pressed down.";
+        break;
+
+      case "mouseup":
+        btn.innerHTML += "...and released.";
+        break;
+    }
+  }
+}
+
+let menu = new Menu();
+
+btn.addEventListener("mouseup", menu);
+btn.addEventListener("mousedown", menu);
