0203a90a7789ca94f6dd6807bca77880ffdaa37f
Abhaya <shankarabhaya9@gmail.com>
2022-11-14 11:14:15 +0530

    Dispatch events

--- MouseEvents/Onclick/Event Delegation/script.js
+++ MouseEvents/Onclick/Event Delegation/script.js
@@ -4,3 +4,22 @@ document.querySelector("#category").addEventListener("click", (e) => {
     window.location.href = "/" + e.target.id;
   }
 });
+
+//Dispatch events script code --
+
+function hide() {
+  let event = new CustomEvent("hide", {
+    cancelable: true, // without that flag preventDefault doesn't work
+  });
+  if (!rabbit.dispatchEvent(event)) {
+    alert("The action was prevented by a handler");
+  } else {
+    rabbit.hidden = true;
+  }
+}
+
+rabbit.addEventListener("hide", function (event) {
+  if (confirm("Call preventDefault?")) {
+    event.preventDefault();
+  }
+});
