d2e843c286cc5a174978a55f4befcd14b4bafb41
Abhaya <shankarabhaya9@gmail.com>
2022-11-16 12:33:34 +0530

    form and methods

---
+++ Form Events/document forms and elements/script.js
@@ -0,0 +1,15 @@
+let form = document.forms.myform;
+let element = form.elements.one;
+// alert(element.value);
+// alert(element[0]);
+
+let form1 = document.forms[0];
+console.log(form1);
+
+let form2 = document.secondForm;
+console.log(form2);
+
+console.log(form2.elements.cheese);
+// alert(form2.elements.paneer);
+
+anime.options[2].selected = true;
